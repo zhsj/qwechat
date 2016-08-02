f232c57af90391a93c957cf85e570c6b1aa1043b
Shengjing Zhu <zsj950618@gmail.com>
2016-08-02 15:59:20 +0800

    add wechat debug url and disable proxy by default

--- qwechat/config.py
+++ qwechat/config.py
@@ -1,11 +1,14 @@
 import os
 
-WX_URL = "https://wx.qq.com"
+WX_URL = "https://wx.qq.com/"
 APP_NAME = "QWeChat"
 NOTIFY_TIMEOUT = 5000
 DEBUG = False
 
-PROXY_ENABLE = True
+if DEBUG:
+    WX_URL = WX_URL + '?mmdebug'
+
+PROXY_ENABLE = False
 PROXY_INFO = ("socks5", "127.0.0.1", 1080)
 
 icon_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
